5daaec1bf0288136723392ba1fe2285dec8e1697
Vidawater <77567124+Vidawater@users.noreply.github.com>
2021-10-14 10:43:40 +0700

    Create start.bat

---
+++ start.bat
@@ -0,0 +1,23 @@
+@echo off
+del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
+net config server /srvcomment:"Windows Server 2019 RDP by Vidawater" > out.txt 2>&1
+REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
+REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /f /v Wallpaper /t REG_SZ /d D:\a\wallpaper.bat
+net user administrator Vidawater /add >nul
+net localgroup administrators administrator /add >nul
+net user administrator /active:yes >nul
+net user installer /delete
+diskperf -Y >nul
+sc config Audiosrv start= auto >nul
+sc start audiosrv >nul
+ICACLS C:\Windows\Temp /grant administrator:F >nul
+ICACLS C:\Windows\installer /grant administrator:F >nul
+echo Installation Success!, if the rdp ded, please rebuild it
+echo IP:
+tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "i cannot get any tunnel, please make sure that Ngrok_Auth_token is in secret or it correct you can check that you vm still run in: https://dashboard.ngrok.com/status/tunnels "
+echo Username: administrator
+echo Password: Vidawater
+echo .
+echo RDP HAS BEEN CREATED.
+echo u can login into it!!
+ping -n 10 127.0.0.1 >nul
